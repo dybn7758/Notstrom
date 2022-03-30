4d0c00e70d2e9a0bb17a765aeb6c00f59a94426a
Tony Duong <Tonyttd@yahoo.com>
2022-03-30 13:04:37 -0500

    Established a base template

---
+++ himalayas-fec/client/src/Components/Related Products/RelatedProducts.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RelatedProducts extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+  return (
+      <div>STUFF</div>
+    )
+  }
+}
+
+export default RelatedProducts;
