87eb5b2b2c4a1da94c2959ba1dc91f55dbc7d402
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-03-30 15:00:08 -0400

    More Pseudo

---
+++ himalayas-fec/client/src/Components/Related Products/Card.jsx
@@ -0,0 +1,25 @@
+// will be used to assemble individual product cards
+// should include
+  // Should be clickable
+    // Should pass off to product detail page
+      // Modal?  Jump to top of page?
+  // Image
+    // primary image of the product from overview
+      // upon hover a sub-carousel should load on the bottom of the image
+        // list should disappear on hover-off
+    // Favorite 'Star'
+  // Footer with
+    // Category
+    // Product Name
+    // Price
+      // pricing by style?
+      // if on sale, should strikethrough OG price, list new price after
+    // Star Rating System
+      // total rating out of 5
+      // should be reflected by 5 partially filled stars
+        // rating should be rounded to nearest quarter rating point
+      // if there are no reviews, no stars shoul appear
+
+// When a carousel item is 'favorited' it should be added to 'outfit'
+  // same design as the carousel
+    // rendered dynamically by click
