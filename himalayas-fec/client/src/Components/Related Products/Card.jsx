046929a8a4ce9ff608282a3cf7deb90e588cbfdd
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-03-30 15:26:40 -0400

    More Pseudo for Cards/Carousel

--- himalayas-fec/client/src/Components/Related Products/Card.jsx
+++ himalayas-fec/client/src/Components/Related Products/Card.jsx
@@ -1,13 +1,20 @@
 // will be used to assemble individual product cards
 // should include
+
   // Should be clickable
     // Should pass off to product detail page
       // Modal?  Jump to top of page?
+
   // Image
     // primary image of the product from overview
       // upon hover a sub-carousel should load on the bottom of the image
+        // there should be 4 sub-images in the sub-carousel
+          // they should be able to be left/right scrolled with buttons
+          // clicking a thumbnail should change preview image
+            // selected image should persist after hover off
         // list should disappear on hover-off
     // Favorite 'Star'
+
   // Footer with
     // Category
     // Product Name
