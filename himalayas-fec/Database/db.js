8001a4d66f2d17a0e2b0e4cba323ff7a6cd7c1eb
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-03-31 13:39:36 -0500

    commit to successfully merge master

--- himalayas-fec/Database/db.js
+++ himalayas-fec/Database/db.js
@@ -1,52 +1,52 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-main().catch(err => console.log(err));
+// main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/fec');
-}
+// async function main() {
+//   await mongoose.connect('mongodb://localhost:27017/fec');
+// }
 
-//make a schema for your data set found in api/your_module/ in Learn
+// //make a schema for your data set found in api/your_module/ in Learn
 
-//Overview
-const productSchema = new mongoose.Schema({
-  id: Number,
-  name: String,
-  slogan: String,
-  description: String,
-  category: String,
-  default_price: String
-})
+// //Overview
+// const productSchema = new mongoose.Schema({
+//   id: Number,
+//   name: String,
+//   slogan: String,
+//   description: String,
+//   category: String,
+//   default_price: String
+// })
 
-//Reviews
+// //Reviews
 
-//Q & A
+// //Q & A
 
-//Related Products
-
-
-
-//make a model using your schema
-
-//Overview
-const Product = mongoose.model('Product', productSchema);
-
-//Reviews
-
-//Q&A
-
-//Related Products
+// //Related Products
 
 
 
-//export your model
+// //make a model using your schema
 
-//Product Detail
-module.exports.Product = Product;
+// //Overview
+// const Product = mongoose.model('Product', productSchema);
 
-//Reviews
+// //Reviews
 
-//Q & A
+// //Q&A
 
-//Related Products
+// //Related Products
+
+
+
+// //export your model
+
+// //Product Detail
+// module.exports.Product = Product;
+
+// //Reviews
+
+// //Q & A
+
+// //Related Products
 
--- himalayas-fec/client/src/App.jsx
+++ himalayas-fec/client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import RelatedProducts from './Components/Related Products/RelatedProducts.jsx';
+import Overview from './Components/Overview/Overview.jsx';
 
 export default class App extends React.Component {
   constructor(props) {
