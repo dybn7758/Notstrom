c3aa383d5cbd04d523b348120b9d77d626918796
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-04-04 10:37:37 -0400

    Git Issues

---
+++ himalayas-fec/client/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import RelatedProducts from './Components/Related Products/RelatedProducts.jsx';
+
+import Overview from './Components/Overview/Overview.jsx';
+
+import QA from './Components/Question_Answers/qa.jsx';
+
+
+ export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+import axios from 'axios';
+import {listQuestions, listProducts, listReviews} from './lib/searchAPI.js';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  userRecoilValue,
+} from 'recoil';
+
+var App = () => {
+
+  //do out API calls to retrieve data using useEffect(callback, [])
+    //pass the second argument so it doesnt create an infinite loop everytime this component renders
+  useEffect(() => {
+    listQuestions(37311); //API call to get questions
+  }, []);
+
+  return (
+    <RecoilRoot>
+      <div> Himalayas For The Win
+        <Overview />
+        <RelatedProducts/>
+        <QA/>
+      </div>
+    </RecoilRoot>
+  )
+};
+
+export default App;
