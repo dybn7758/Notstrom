e39b7ddc8b013c9e2514c4bfa59babbbdc3d4736
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-04-11 10:10:18 -0500

    merge master

--- himalayas-fec/webpack.config.js
+++ himalayas-fec/webpack.config.js
@@ -15,9 +15,20 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /nodeModules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+       {
+        test: /\.s[ac]ss$/i,
         use: [
-          loader: "babel-loader", "css-loader"
-        ]
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
