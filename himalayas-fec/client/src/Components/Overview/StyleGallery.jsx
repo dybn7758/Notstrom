59c1c218b127cc66283fa5195d439854dfdc0725
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-04-13 14:53:49 -0500

    formatting/styling started

--- himalayas-fec/client/src/Components/Overview/StyleGallery.jsx
+++ himalayas-fec/client/src/Components/Overview/StyleGallery.jsx
@@ -3,45 +3,52 @@ import StyleDisplay from './StyleDisplay.jsx';
 
 const StyleGallery = (props) => {
 
-  const [imageSelection, selectImage] = useState(props.style.photos[0].url)
+  if (props.style.photos[0].url) {
 
-  useEffect(() => {
-    selectImage(props.style.photos[0].url);
-  }, [props.style])
+    const [imageSelection, selectImage] = useState(props.style.photos[0].url)
 
-  const imageSelector = (targetUrl) => {
-    selectImage(targetUrl)
-  }
+    useEffect(() => {
+      selectImage(props.style.photos[0].url);
+    }, [props.style.photos])
 
-    const photos = props.style.photos;
+    const imageSelector = (targetUrl) => {
+      selectImage(targetUrl);
+    }
+
+      const photos = props.style.photos;
+
+      return (
+        <div className="gallery">
+          <div className="galleryThumbs">
+            <h2>Fashion Gallery</h2>
+            {photos.map((photo, index) => {
+              return(
+                <div className="gallery-cell"
+                key={index}>
+                  <img src={photo.thumbnail_url} width="75" height="90"
+                    url={photo.url}
+                    onClick={event => imageSelector(event.target.attributes.url.nodeValue)}></img>
+                </div>
+              )
+            })}
+            <div className="display">
+              <StyleDisplay image={imageSelection} />
+            </div>
+          </div>
+        </div>
+      )
+
+  } else {
 
     return (
       <div className="gallery">
-        <h2>Fashion Gallery</h2>
-        {photos.map((photo, index) => {
-          return(
-            <div className="gallery-cell"
-            key={index}>
-              <img src={photo.thumbnail_url} width="75" height="90"
-                url={photo.url}
-                onClick={event => imageSelector(event.target.attributes.url.nodeValue)}></img>
-            </div>
-          )
-        })}
-        <StyleDisplay image={imageSelection} />
+        <img src="https://i.redd.it/xke2yxfzofu71.jpg" alt="Hang In There!" width="300" height="300"></img>
       </div>
     )
   }
+}
 
 
 export default StyleGallery;
 
 
-// <div class="gallery js-flickity"
-//   data-flickity-options='{ "wrapAround": true }'>
-//   <div class="gallery-cell"></div>
-//   <div class="gallery-cell"></div>
-//   <div class="gallery-cell"></div>
-//   <div class="gallery-cell"></div>
-//   <div class="gallery-cell"></div>
-// </div>
